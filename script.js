ac5dc9b16753afeed76eb423df870a52a56edeb8
ineveu <65143026+ineveu@users.noreply.github.com>
2024-01-30 19:32:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const videos = [
-    { url: 'https://youtu.be/vApPNAZneLM?si=VLe_j5WHIg_Fuf4L', tags: ['Motion Graphics', 'Reel'] },
-    { url: 'https://player.vimeo.com/video/VIDEO_ID_2', tags: ['tag1'] },
+    { embedCode: '<iframe width="560" height="315" src="https://www.youtube.com/embed/VIDEO_ID_1" frameborder="0" allowfullscreen></iframe>', tags: ['tag1', 'tag2'] },
+    { embedCode: '<iframe width="560" height="315" src="https://www.youtube.com/embed/VIDEO_ID_2" frameborder="0" allowfullscreen></iframe>', tags: ['tag1'] },
     // Add more videos with respective tags
 ];
 
@@ -11,7 +11,7 @@ function renderVideos(filteredVideos) {
     filteredVideos.forEach(video => {
         const videoItem = document.createElement('div');
         videoItem.classList.add('video-item');
-        videoItem.innerHTML = `<iframe src="${video.url}" frameborder="0" allowfullscreen></iframe>
+        videoItem.innerHTML = `${video.embedCode}
                               <div class="video-title">${video.tags.join(', ')}</div>`;
         grid.appendChild(videoItem);
     });
