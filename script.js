e53d4f751aa94c8a9638be1d938848278eb9df18
ineveu <65143026+ineveu@users.noreply.github.com>
2024-01-30 19:32:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const videos = [
-    { embedCode: '<iframe width="560" height="315" src="https://www.youtube.com/embed/VIDEO_ID_1" frameborder="0" allowfullscreen></iframe>', tags: ['tag1', 'tag2'] },
+    { embedCode: '<iframe width="560" height="315" src="<div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/201187557?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Motion Graphics Reel by Ivan Neveu"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>" frameborder="0" allowfullscreen></iframe>', tags: ['tag1', 'tag2'] },
     { embedCode: '<iframe width="560" height="315" src="https://www.youtube.com/embed/VIDEO_ID_2" frameborder="0" allowfullscreen></iframe>', tags: ['tag1'] },
     // Add more videos with respective tags
 ];
