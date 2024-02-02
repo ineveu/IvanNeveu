fcc2db794946b014c6f514f8a23b9200fdd616b7
ineveu <65143026+ineveu@users.noreply.github.com>
2024-02-01 22:16:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
+// List of videos with corresponding embed codes, tags, and Vimeo video IDs
 const videos = [
-    { embedCode: '<div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/201187557?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Motion Graphics Reel by Ivan Neveu"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>', tags: ['reels', 'motion graphics',] },
-    { embedCode: '<div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/90584639?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"  frameborder="0" allow="autoplay; fullscreen; picture-in-picture" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Character Animation Reel by Ivan Neveu"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>', tags: ['reels', 'character animation',] },
-    { embedCode: '<div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/393211584?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Science Giants!"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>', tags: ['design', 'character animation',] },
-    { embedCode: '<div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/236523917?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Wolf Birds Motion Test"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>', tags: ['character animation'] },
-    { embedCode: '<div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/182959783?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Halloween Bumper by Ivan Neveu"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>', tags: ['motion graphics', 'design'] },
-    { embedCode: '<div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/475709830?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Erectoral College"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>', tags: ['editing', 'design'] },
-    { embedCode: '<div style="padding:66.67% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/226944712?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Milk3D_Making_Of"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>', tags: ['editing'] },
-    { embedCode: '<div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/226943813?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Starbucks 1 Cup Making Of"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>', tags: ['editing'] },
-    { embedCode: '<div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/141129446?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="MilkShake"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>', tags: ['editing', 'design'] },
-	
-   // Add more videos with respective tags
+    { embedCode: '<img src="images/image-201187557.png" alt="Image 1">', tags: ['demo', 'reel', 'motion graphics'], vimeoVideoId: '201187557' },
+    { embedCode: '<img src="images/image-90584639.png" alt="Image 2">', tags: ['demo','reel', 'character animation'], vimeoVideoId: '90584639' },
+	{ embedCode: '<img src="images/image-89743717.png" alt="Image 2">', tags: ['demo','reel', 'compositing / vfx'], vimeoVideoId: '89743717' },
+    { embedCode: '<img src="images/image-393211584.png" alt="Image 1">', tags: ['show pitch','design', 'character animation'], vimeoVideoId: '393211584' },
+	{ embedCode: '<img src="images/image-908917822.png" alt="Image 2">', tags: ['character animation'], vimeoVideoId: '908917822' },
+    { embedCode: '<img src="images/image-236523917.png" alt="Image 2">', tags: ['character animation'], vimeoVideoId: '236523917' },
+    { embedCode: '<img src="images/image-182959783.png" alt="Image 1">', tags: ['motion graphics', 'design'], vimeoVideoId: '182959783' },
+    { embedCode: '<img src="images/image-475709830.png" alt="Image 2">', tags: ['editing', 'design'], vimeoVideoId: '475709830' },
+    { embedCode: '<img src="images/image-226944712.png" alt="Image 2">', tags: ['making of doc','editing'], vimeoVideoId: '226944712' },
+    { embedCode: '<img src="images/image-226943813.png" alt="Image 2">', tags: ['making of doc','editing'], vimeoVideoId: '226943813' },
+    { embedCode: '<img src="images/image-141129446.png" alt="Image 2">', tags: ['pitch','editing', 'design'], vimeoVideoId: '141129446' },
+    { embedCode: '<img src="images/image-908465813.png" alt="Image 2">', tags: ['compositing / vfx'], vimeoVideoId: '908465813' },
+    { embedCode: '<img src="images/image-908468721.png" alt="Image 2">', tags: ['compositing / vfx'], vimeoVideoId: '908468721' },
+    { embedCode: '<img src="images/image-908470537.png" alt="Image 2">', tags: ['compositing / vfx'], vimeoVideoId: '908470537' },
+    { embedCode: '<img src="images/image-908471601.png" alt="Image 2">', tags: ['compositing / vfx'], vimeoVideoId: '908471601' },
+    { embedCode: '<img src="images/image-908473397.png" alt="Image 2">', tags: ['character animation'], vimeoVideoId: '908473397' },
+    { embedCode: '<img src="images/image-908475904.png" alt="Image 2">', tags: ['character animation'], vimeoVideoId: '908475904' },
+    { embedCode: '<img src="images/image-908478390.png" alt="Image 2">', tags: ['character animation'], vimeoVideoId: '908478390' },
+    { embedCode: '<img src="images/image-908915937.png" alt="Image 2">', tags: ['compositing / vfx'], vimeoVideoId: '908915937' },
+	{ embedCode: '<img src="images/image-908920789.png" alt="Image 2">', tags: ['character animation'], vimeoVideoId: '908920789' },
+	{ embedCode: '<img src="images/image-908922600.png" alt="Image 2">', tags: ['compositing / vfx'], vimeoVideoId: '908922600' },
+
+    // Add more images with respective tags and Vimeo video IDs
 ];
 
+// Function to render videos based on the selected tag
 function renderVideos(filteredVideos) {
     const grid = document.getElementById('video-grid');
     grid.innerHTML = '';
@@ -22,14 +36,71 @@ function renderVideos(filteredVideos) {
         videoItem.innerHTML = `${video.embedCode}
                               <div class="video-title">${video.tags.join(', ')}</div>`;
         grid.appendChild(videoItem);
+
+        // Add click event to each video item to open the corresponding Vimeo video
+        videoItem.addEventListener('click', () => openVideoModal(video.vimeoVideoId));
     });
 }
 
+// Function to filter videos based on the selected tag
 function filterVideos(tag) {
     const filteredVideos = tag === 'all' ? videos : videos.filter(video => video.tags.includes(tag));
     renderVideos(filteredVideos);
 }
 
-// Initial rendering
+// Function to open Vimeo video in modal
+function openVideoModal(vimeoVideoId) {
+    const modal = document.getElementById('modal');
+    modal.classList.add('open');
+
+    // Use the Vimeo API to load the video into the iframe
+    const iframe = document.getElementById('vimeo-player');
+    iframe.src = `https://player.vimeo.com/video/${vimeoVideoId}?autoplay=1&loop=0&byline=0&portrait=0`;
+
+    // Close the modal when the video ends
+    iframe.addEventListener('ended', closeVideoModal);
+}
+
+// Function to close the video modal
+function closeVideoModal() {
+    const modal = document.getElementById('modal');
+    modal.classList.remove('open');
+
+    // Reset the iframe src to stop the video
+    const iframe = document.getElementById('vimeo-player');
+    iframe.src = '';
+}
+
+// Initial rendering with all videos
 filterVideos('all');
 
+// Add an event listener for the close button
+document.getElementById('close-modal-btn').addEventListener('click', closeVideoModal);
+
+// Function to open Vimeo video in modal
+function openVideoModal(vimeoVideoId) {
+    const modal = document.getElementById('modal');
+    modal.classList.add('open');
+
+    // Use the Vimeo API to load the video into the iframe
+    const iframe = document.getElementById('vimeo-player');
+    iframe.src = `https://player.vimeo.com/video/${vimeoVideoId}?autoplay=1&loop=0&byline=0&portrait=0`;
+
+  // Set the style for the iframe to force the desired size (e.g., width: 800px; height: 450px;)
+    iframe.style.width = '80%'; // Adjust the width as needed
+    iframe.style.height = '450px'; // Adjust the height as needed
+
+
+    // Close the modal when the close button or video ends
+    iframe.addEventListener('ended', closeVideoModal);
+}
+
+// Function to close the video modal
+function closeVideoModal() {
+    const modal = document.getElementById('modal');
+    modal.classList.remove('open');
+
+    // Reset the iframe src to stop the video
+    const iframe = document.getElementById('vimeo-player');
+    iframe.src = '';
+}
